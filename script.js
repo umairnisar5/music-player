81805727d16b0b698fed122ef6de401e45b01765
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-24 19:24:01 +0500

    update script

---
+++ script.js
@@ -0,0 +1,15 @@
+const musicContainer = document.getElementById('music_container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressConatiner = document.getElementById('progress-conatiner');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+
+// song title 
+const songs = ['hey', 'summer', 'ukulele']
